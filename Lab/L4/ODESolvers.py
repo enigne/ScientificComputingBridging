66899e95eea2027bce1725908c59db390ca46a8a
Cheng Gong <cheng.gong@it.uu.se>
2019-09-24 14:19:30 +0200

    add Heuns method

--- Lab/L4/ODESolvers.py
+++ Lab/L4/ODESolvers.py
@@ -33,3 +33,27 @@ def EulerForward(func, t_span, y0, n):
         y.append(ynp1)
     return t, y
     
+    
+def Heun(func, t_span, y0, n):
+    """ Heun's method
+    Parameters
+    ----------
+    func : function, function handler of the right hand side of the ODE(s);
+    t_span : list, t_span[0] is the initial time, t_span[1] is the final time;
+    y0 : float, list, initial condition(s) of the ODE(s);
+    n : number of time steps, dt = (t_span[1]-t_span[0])/n.
+        
+    Returns
+    -------
+    t : list, the resulting time steps;
+    y : list, the solutions of the ODE(s)
+    """
+    t = np.linspace(t_span[0], t_span[1], n)
+    dt = t[1]-t[0]
+    y = [y0]
+    for i in range(n-1):
+        k1 = func(t[i], y[i])
+        k2 = func(t[i+1], y[i]+dt*k1)
+        ynp1 = y[i] + 0.5*dt*(k1+k2)
+        y.append(ynp1)
+    return t, y
